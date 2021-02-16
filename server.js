9e719862d95ce01a281554af380b35122ab49d1e
MohammadRayanShah <rayanshah379@gmail.com>
2021-02-16 13:50:33 +0530

    Fly

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const bodyparser = require("body-parser");
+const path = require("path");
+
+//log Request using morgan
+app.use(morgan("tiny"));
+
+//parse request to body-parser
+app.use(bodyparser.urlencoded({ extended: true }));
+
+//set view engine
+app.set("view engine", "ejs");
+
+//load assets  "have to use only /css/style.css "
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+//Get Method
+app.get("/", (req, res) => {
+  res.render("index.ejs"); //helps us to render a HTML file
+});
+
+//using dotenv to change port
+dotenv.config({ path: "config.env" });
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`listening on http://localhost:${PORT}`);
+});
